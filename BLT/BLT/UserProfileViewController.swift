5cbeee896f6d38e6030dcb3a2e5ca9a4b90e398e
dlloydgeorge <32097513+dlloydgeorge@users.noreply.github.com>
2020-01-15 23:50:53 -0500

    Super Basic set up for profile page
    Dunno if this is what you want ? will do more later.

---
+++ BLT/BLT/UserProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UserProfileViewController.swift
+//  BLT
+//
+//  Created by Student on 1/15/20.
+//  Copyright © 2020 BLT Group. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+}
