48ba3dd33ed15c8d3038c6cb07d91b4f7b167fbf
Nicholas Lorentzen <nicholas.lorentzen@gmail.com>
2020-01-18 14:58:02 -0500

    Add Old Swift Files

---
+++ BLT/BLT/RenderTimer.swift
@@ -0,0 +1,31 @@
+//
+//  RenderTimer.swift
+//  BLT
+//
+//  Created by LorentzenN on 12/3/19.
+//  Copyright © 2019 Lorentzen. All rights reserved.
+//
+
+import Foundation
+class RenderTimer {
+    var myTimer : Timer
+    
+    weak var delegate: RenderTimerDelegate?
+    
+    init() {
+        myTimer = Timer()
+    }
+    
+    func runTimer(interval : Double) {
+        myTimer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(updateVals), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateVals() {
+        delegate?.render()
+    }
+}
+
+protocol RenderTimerDelegate : class {
+    ///Reminds other class to re draw
+    func render()
+}
