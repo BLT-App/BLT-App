c338b9c707db3d2dfb769d38e1d55c808a7837a7
Nicholas L <33159891+EKKOING@users.noreply.github.com>
2020-01-27 00:10:19 -0500

    Settings Page Full Redesign (#26)
    * Basic Storyboard Setup
    
    Adds the Settings Tab and Settings Submenu View Controllers
    
    Settings Page has 4 Submenu categories
    
    * Remove Old View Controllers
    
    * Remove Blank View Controller
    
    * Reorganize Files
    
    * Add basic functions
    
    * Swap To Static Views
    
    * Finish Basic Storyboard Layout
    
    * Working Settings

---
+++ BLT/BLT/Settings Views/SettingsFocusPrefSubmenuTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingsFocusPrefSubmenuTableViewController.swift
+//
+//
+//  Created by LorentzenN on 1/26/20.
+//
+
+import UIKit
+
+class SettingsFocusPrefSubmenuTableViewController: UITableViewController {
+    
+    @IBOutlet weak var switchShowEndButton: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        switchShowEndButton.isOn = globalData.includeEndFocusButton
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        globalData.includeEndFocusButton = switchShowEndButton.isOn
+        
+        globalData.saveUserData()
+    }
+    
+}
---
+++ BLT/BLT/Settings Views/SettingsListPrefSubmenuTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsListPrefSubmenuTableViewController.swift
+//
+//
+//  Created by LorentzenN on 1/26/20.
+//
+
+import UIKit
+
+class SettingsListPrefSubmenuTableViewController: UITableViewController {
+    @IBOutlet weak var switchSortList: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        switchSortList.isOn = globalData.wantsListByDate
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        globalData.wantsListByDate = switchSortList.isOn
+        
+        globalData.saveUserData()
+    }
+    
+}
