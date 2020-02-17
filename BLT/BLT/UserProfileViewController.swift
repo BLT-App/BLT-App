d3dc18371b3bb7e49c94cc3a343e31864bc5bc81
Jiahua Chen <jc@jh.tc>
2020-02-17 01:37:22 -0500

    Better profile page (#35)
    * Revert "Revert "Better Profile Page""
    
    This reverts commit 7e4d6c319fd103655f98aa612480fa91a2da3e55.
    
    * Ignore LineChart.swift in SwiftLint Runs
    
    LineChart.swift is not our code, so it doesn't make sense to refactor it.
    
    * Edited out old code
    
    Co-authored-by: Nicholas L <33159891+EKKOING@users.noreply.github.com>

--- BLT/BLT/UserProfileViewController.swift
+++ BLT/BLT/UserProfileViewController.swift
@@ -12,32 +12,26 @@ import Charts
 class UserProfileViewController: UIViewController
 {
     
-    @IBOutlet weak var userImage: UIImageView!
-    
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-    @IBOutlet weak var userDataChart: BarChartView!
-    
-    @IBOutlet weak var label: UILabel!
-    
     @IBOutlet weak var userNameLabel: UILabel!
     
-    func barChartUpdate(){
-        var list: [BarChartDataEntry] = []
-        for index in 0 ... 6{
-            list.append(BarChartDataEntry(x: Double(index), y: Double(Int.random(in: 0..<10))))
-        }
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var shadowView: UIView!
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var graphShadowView: UIView!
+    
+    @IBOutlet weak var graphContainerView: UIView!
+    
+    @IBOutlet weak var tasksCompletedChart: LineChart!
+    
+    func chartUpdate(){
+        var trendData: [CGFloat] = [5, 2, 7, 8, 3, 5]
+        tasksCompletedChart.clear()
+        tasksCompletedChart.addLine(trendData)
+        tasksCompletedChart.y.grid.count = 5
         
-        let dataSet = BarChartDataSet(values: list, label: "Tasks completed")
-        let data = BarChartData(dataSets: [dataSet])
-        
-        userDataChart.data = data
-        userDataChart.chartDescription?.text = "Tasks completed"
-        
-        //All other additions to this function will go here
-        
-        //This must stay at end of function
-        userDataChart.notifyDataSetChanged()
     }
     
     override func viewDidLoad() {
@@ -46,8 +40,17 @@ class UserProfileViewController: UIViewController
         //the view, typically from a nib.
         prepareProfile()
         
-        barChartUpdate()
+        setupCards()
         
+        chartUpdate()
+        
+    }
+    
+    func setupCards() {
+        roundContainerView(cornerRadius: 20, view: containerView, shadowView: shadowView)
+        addShadow(view: shadowView, color: UIColor.gray.cgColor, opacity: 0.2, radius: 10, offset: CGSize(width: 0, height: 5))
+        roundContainerView(cornerRadius: 20, view: graphContainerView, shadowView: graphShadowView)
+        addShadow(view: graphShadowView, color: UIColor.gray.cgColor, opacity: 0.2, radius: 10, offset: CGSize(width: 0, height: 5))
     }
     
     
@@ -56,30 +59,43 @@ class UserProfileViewController: UIViewController
         userImage.clipsToBounds = true
         userImage.layer.cornerRadius = 50
         
-        userDataChart.clipsToBounds = true
-        userDataChart.layer.cornerRadius = 25
-        userDataChart.backgroundColor = .gray
+        userNameLabel.text = "\(globalData.firstName) \(globalData.lastName)"
+    }
+}
+
+extension UserProfileViewController {
+    /**
+     Creates a rounded container view.
+     - parameters:
+     - cornerRadius: The corner radius of the rounded container.
+     - view: The UIView to round.
+     - shadowView: The accompanying shadowView of the main view to round.
+     */
+    func roundContainerView(cornerRadius: Double, view: UIView, shadowView: UIView) {
+        let path = UIBezierPath(roundedRect: view.bounds, byRoundingCorners: [.topLeft, .topRight, .bottomLeft, .bottomRight], cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = view.bounds
+        maskLayer.path = path.cgPath
+        view.layer.mask = maskLayer
         
-        scrollView.layer.cornerRadius = 25
-        scrollView.backgroundColor = .lightGray
-        
-        label.backgroundColor = .blue
-        label.clipsToBounds = true
-        label.layer.cornerRadius = 10
-        
-        
-        userNameLabel.clipsToBounds = true
-        userNameLabel.layer.cornerRadius = 5
-        userNameLabel.backgroundColor = .blue
-        if let firstName: String = globalData.firstName {
-            userNameLabel.text = firstName
-        }
+        shadowView.layer.cornerRadius = CGFloat(cornerRadius)
+        shadowView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMinYCorner]
     }
     
-    
-    
-    
-    
-    
-    
+    /**
+     Creates shadows for a view.
+     - parameters:
+     - view: The view to add a shadow to.
+     - color: The color of the shadow.
+     - opacity: The opacity of the shadow.
+     - radius: The radius of the shadow.
+     - offset: The offset of the shadow.
+     */
+    func addShadow(view: UIView, color: CGColor, opacity: Float, radius: CGFloat, offset: CGSize) {
+        view.layer.shadowColor = color
+        view.layer.shadowOpacity = opacity
+        view.layer.shadowOffset = offset
+        view.layer.shadowRadius = radius
+        view.layer.masksToBounds = false
+    }
 }
