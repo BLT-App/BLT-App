78589d3d6e43e284fccc50253a84f4b45bd2a67b
Nicholas Lorentzen <nicholas.lorentzen@gmail.com>
2020-05-14 17:10:18 -0400

    Fix File Linkage Issue

--- BLT/BLT/ActivityTableViewCell.swift
+++ BLT/BLT/ActivityTableViewCell.swift
@@ -1,25 +1,42 @@
 //
 //  ActivityTableViewCell.swift
-//  BLT
+//  
 //
-//  Created by LorentzenN on 5/11/20.
-//  Copyright © 2020 BLT App. All rights reserved.
+//  Created by LorentzenN on 5/14/20.
 //
 
 import UIKit
 
 class ActivityTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var timeStamp: UILabel!
-    @IBOutlet weak var eventDescription: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
+    
+    var event: DatabaseEvent? {
+        didSet {
+            lblDescription.text = event?.eventText
+            let numHour = (event?.date.currentCalendar.components.hour ?? 0) % 12
+            let numMin = event?.date.currentCalendar.components.minute ?? 0
+            var textMin = "\(numMin)"
+            if numMin < 10 {
+                textMin = "0\(numMin)"
+            }
+            var indicator = "am"
+            if event?.date.currentCalendar.components.hour ?? 0 >= 12 {
+                indicator = "pm"
+            }
+            lblTime.text = "\(numHour):\(textMin) \(indicator)"
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        event = DatabaseEvent()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        
     }
-    
+
 }
